6d70d94ee8b2f3818d8d50181bafc38c55bc608c
Abdul Rehman <naeemabdulrehman08@gmail.com>
2025-04-22 17:07:17 +0500

    Time: 5 ms (47.92%), Space: 58.6 MB (21.96%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,14 +3,17 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    const frequency = new Map()
-    nums.forEach(item =>{
-        frequency.set(item, (frequency.get(item) || 0) + 1);
-    })
-    const threshold = nums.length/2
-    for (const [num, count] of frequency) {
-        if (count > threshold) {
-            return num; 
-        }
-    }
+    // const frequency = new Map()
+    // nums.forEach(item =>{
+    //     frequency.set(item, (frequency.get(item) || 0) + 1);
+    // })
+    // const threshold = nums.length/2
+    // for (const [num, count] of frequency) {
+    //     if (count > threshold) {
+    //         return num; 
+    //     }
+    // }
+
+    nums.sort((a, b) => a - b);
+    return nums[Math.floor(nums.length / 2)]
 };
