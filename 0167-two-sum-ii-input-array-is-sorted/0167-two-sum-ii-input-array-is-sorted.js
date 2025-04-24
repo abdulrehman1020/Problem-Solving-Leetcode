ceb195c879ca09b4e43a80f5de05a9131062a9ce
Abdul Rehman <naeemabdulrehman08@gmail.com>
2025-04-24 14:59:56 +0500

    Time: 1 ms (84%), Space: 54.9 MB (86.43%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -6,13 +6,14 @@
 var twoSum = function(numbers, target) {
     let left = 0;
     let right = numbers.length - 1;
-    while(left <= right){
+    while(left<right){
         let sum = numbers[left] + numbers[right];
-        if(sum == target) return [left + 1, right + 1]
-        if(sum > target){
+        if(sum == target){
+            return [left+1, right+1];
+        }else if(sum > target){
             right--;
         }else{
-            left++;
+            left++
         }
     }
 };
