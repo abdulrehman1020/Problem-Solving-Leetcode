8afc676843f5e4350f19a9f58a587b8c16818313
Abdul Rehman <naeemabdulrehman08@gmail.com>
2025-04-25 20:27:09 +0500

    Time: 3 ms (66.29%), Space: 65.6 MB (25.06%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,14 +3,16 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-     let buy = prices[0];
-        let profit = 0;
-        for (let i = 1; i < prices.length; i++) {
-            if (prices[i] < buy) {
-                buy = prices[i];
-            } else if (prices[i] - buy > profit) {
-                profit = prices[i] - buy;
-            }
+  let buyPrice = prices[0];
+    let profit = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        if (buyPrice > prices[i]) {
+            buyPrice = prices[i];
         }
-        return profit;
+
+        profit = Math.max(profit, prices[i] - buyPrice);
+    }
+
+    return profit;  
 };
