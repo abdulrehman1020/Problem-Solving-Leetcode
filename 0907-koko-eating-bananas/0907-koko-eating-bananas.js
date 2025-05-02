7ae53262fb59d15d931ef24315b06ac02816ede7
Abdul Rehman <naeemabdulrehman08@gmail.com>
2025-05-03 03:55:14 +0500

    Time: 13 ms (27.67%), Space: 58.5 MB (41.98%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function(piles, h) {
+    function check(piles, h, mid) {
+        let ans = 0;
+        for (let pile of piles) {
+            ans += Math.floor(pile / mid);
+            if (pile % mid !== 0) {
+                ans++;
+            }
+        }
+        return ans <= h;
+    }
+
+    let low = 1;
+    let high = Math.max(...piles);
+
+    while (low < high) {
+        let mid = Math.floor((low + high) / 2);
+        if (check(piles, h, mid)) {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return low;
+};
