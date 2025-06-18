f279b6677369955a050d2bc8b42f2038ed2a6726
Abdul Rehman <naeemabdulrehman08@gmail.com>
2025-06-18 11:52:08 +0500

    Time: 1 ms (32.52%), Space: 56.3 MB (79.39%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var solve =(n, p)=>{
+    let result = 2 ** p
+    if(result == n) return true
+    if(result > n) return false
+    return solve(n, p+1)
+}
+
+var isPowerOfTwo = function(n) {
+    return solve(n, 0)
+    // return false
+};
