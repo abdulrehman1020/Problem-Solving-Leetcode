7c0a684f32d2168960baa7e715bb8985f530ef2c
Abdul Rehman <naeemabdulrehman08@gmail.com>
2025-07-10 07:51:17 +0500

    Time: 5 ms (22.05%), Space: 58.6 MB (86.87%) - LeetHub

--- 0046-permutations/0046-permutations.js
+++ 0046-permutations/0046-permutations.js
@@ -3,16 +3,20 @@
  * @return {number[][]}
  */
 var permute = function(nums) {
-    const result = [];
-    const backtrack = (nums, path) => {
-        if (nums.length === 0) {
-            result.push(path);
+    let res = [];
+
+    function dfs(i) {
+        if (i === nums.length) {
+            res.push([...nums]);
             return;
         }
-        for (let i = 0; i < nums.length; i++) {
-            backtrack([...nums.slice(0, i), ...nums.slice(i + 1)], [...path, nums[i]]);
+        for (let j = i; j < nums.length; j++) {
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            dfs(i + 1);
+            [nums[i], nums[j]] = [nums[j], nums[i]];
         }
-    };
-    backtrack(nums, []);
-    return result;
+    }
+
+    dfs(0);
+    return res;
 };
