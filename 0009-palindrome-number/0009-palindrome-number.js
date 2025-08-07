919c45e8d0c95226a7d0f8edeceb2d648e5d2829
Abdul Rehman <naeemabdulrehman08@gmail.com>
2025-08-07 11:18:51 +0500

    Time: 6 ms (67.11%), Space: 64.3 MB (58.72%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,17 +3,12 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    if (x < 0) {
-        return false;
-    }
-
     let reverse = 0;
-    let xcopy = x;
-
-    while (x > 0) {
-        reverse = (reverse * 10) + (x % 10);
-        x = Math.floor(x / 10);
+    let num = x;
+    while(num>0){
+        let rem = num % 10;
+        reverse = (reverse * 10) + rem;
+        num = Math.floor(num/10);
     }
-
-    return reverse === xcopy;    
+    return x === reverse
 };
